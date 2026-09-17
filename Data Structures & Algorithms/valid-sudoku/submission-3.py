5e85c59c247763e78876d490661970df713f8582
Luke Wu <123105804+lukewu1@users.noreply.github.com>
2026-09-17 13:04:05 -0600

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        seen_col = [set() for i in range(9)]
+        seen3x3 = [set() for i in range(9)]
+        for i in range(9):
+            seen_row = set()
+            for j in range(9):
+                # Row Check
+                if board[i][j] not in seen_row and board[i][j].isdigit():
+                    seen_row.add(board[i][j])
+                elif board[i][j] in seen_row and board[i][j].isdigit():
+                    return False
+                
+                #Col Check
+                if board[i][j] not in seen_col[j] and board[i][j].isdigit():
+                    seen_col[j].add(board[i][j])
+                elif board[i][j] in seen_col[j] and board[i][j].isdigit():
+                    return False
+
+                #3x3 Check
+                print(i, j, (2*(i//3)) + (i//3) + (j//3))
+                if board[i][j] not in seen3x3[(2*(i//3)) + (i//3) + (j//3)] and board[i][j].isdigit():
+                    seen3x3[(2*(i//3)) + (i//3) + (j//3)].add(board[i][j])
+                elif board[i][j] in seen3x3[(2*(i//3)) + (i//3) + (j//3)] and board[i][j].isdigit():
+                    return False
+        return True
