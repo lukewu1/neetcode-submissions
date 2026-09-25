8c9c7db3d3f386b6f67fdb03a2bc993964760ce7
Luke Wu <123105804+lukewu1@users.noreply.github.com>
2026-09-25 12:38:11 -0600

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        need = [0] * 26
+        have = [0] * 26
+
+        for c in s1:
+            need[ord(c) - 97] += 1
+        
+        length = len(s1)
+
+        for i in range(len(s2)):
+            char = s2[i]
+            if i > (length - 1):
+                have[ord(s2[i - length]) - 97] -= 1
+                have[ord(s2[i]) - 97] += 1
+            else:
+                have[ord(s2[i]) - 97] += 1
+            
+            if need == have:
+                return True
+        
+        return False
