660239bd21b220e942340670c461776c2308507f
Luke Wu <123105804+lukewu1@users.noreply.github.com>
2026-09-25 12:40:30 -0600

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        need = [0] * 26
+        have = [0] * 26
+
+        for c in s1:
+            need[ord(c) - 97] += 1
+        
+        length = len(s1)
+
+        for i, c in enumerate(s2):
+            if i >= length:
+                have[ord(s2[i - length]) - 97] -= 1
+
+            have[ord(c) - 97] += 1
+            
+            if need == have:
+                return True
+        
+        return False
