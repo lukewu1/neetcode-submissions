b4094122f750bcb9587cbb431e7f2d8c15472fd3
Luke Wu <123105804+lukewu1@users.noreply.github.com>
2026-09-25 22:46:43 -0600

    Add: minimum-window-with-characters - submission-12

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-12.py
@@ -0,0 +1,41 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        if len(t) == len(s) and Counter(s) != Counter(t):
+            return ""
+
+        seenS = collections.defaultdict(int)
+        seenT = Counter(t)
+        need = len(seenT)
+        have = 0
+
+        l, r = 0, 0
+        index = ()
+        length = float('inf')
+        while r < len(s):
+            while r < len(s) and have != need:
+                if s[r] in t:
+                    seenS[s[r]] += 1
+                    if seenS[s[r]] == seenT[s[r]]:
+                        have += 1
+                r += 1
+
+            while have == need:
+                if (r - l) < length:
+                    index = (l,r)
+                    length = r - l
+                if s[l] in t:
+                    seenS[s[l]] -= 1
+                    if seenS[s[l]] < seenT[s[l]]:
+                        print(seenS, seenT)
+                        have -= 1
+                l += 1
+        
+        if not index:
+            return ""
+        return s[index[0]:index[1]]
+
+
+
+        
